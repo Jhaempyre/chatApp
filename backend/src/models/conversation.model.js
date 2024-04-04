857443f53156a33e24c44047d8531c4eb4e29d2e
jhaemyre <jhaashish.ajha@gmail.com>
2024-04-04 12:05:17 +0530

    all models added

---
+++ backend/src/models/conversation.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const conversationScehma = new  mongoose.Schema({
+    partcipants:[{
+        type : mongoose.Schema.ObjectId,
+        ref: User ,
+        required :true
+    }],
+    messages : [{
+        type : mongoose.Schema.ObjectId,
+        ref : Message ,
+        required : true,
+        default :[]
+    }]
+
+},{
+    timestamps : true}
+    )
+
+    const Conversation = mongoose.model("Conversation", conversationScehma);
+
+    export default  Conversation;
+
